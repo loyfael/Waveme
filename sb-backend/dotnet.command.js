d8b125a68fde909ae959be63fb9b4eb8be2484e9
Loïs D. <felke2101@gmail.com>
2024-06-19 16:59:23 +0200

    Create dotnet.command.js

---
+++ sb-backend/dotnet.command.js
@@ -0,0 +1,4 @@
+// Create dbcontext from database format
+/*
+dotnet ef dbcontext scaffold "Host=10.117.20.153;Database=soonbward;Username=root;Password=root" Npgsql.EntityFrameworkCore.PostgreSQL --context-dir DbContext --output-dir Models
+*/
