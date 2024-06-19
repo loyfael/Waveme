e2c5aac49463c328fbe4ead301cd2929757e2380
Loïs D. <felke2101@gmail.com>
2024-06-19 17:01:12 +0200

    feat(models): create user models

---
+++ sb-backend/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace sb_backend.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public virtual Category? Category { get; set; }
+}
