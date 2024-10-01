08e062ff5f0a4f8548188efbdc24925383389295
Loïs D. <58104051+Ainkra@users.noreply.github.com>
2024-10-01 16:38:48 +0200

    fix(backend): update dependencies

---
+++ waveme-backend/src/test/java/com/alpineblue/waveme_backend/WavemeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpineblue.waveme_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavemeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
