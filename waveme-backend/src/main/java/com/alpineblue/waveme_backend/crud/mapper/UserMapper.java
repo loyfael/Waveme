2a547603cb8c493ccd7914b2c502655d198e7ae9
Loyfael <58104051+loisdps@users.noreply.github.com>
2024-10-03 15:27:38 +0200

    feat(backend:interfaces): add usermapper

---
+++ waveme-backend/src/main/java/com/alpineblue/waveme_backend/crud/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package com.alpineblue.waveme_backend.crud.mapper;
+
+import com.alpineblue.waveme_backend.crud.dto.UserDto;
+import com.alpineblue.waveme_backend.crud.entity.User;
+
+public class UserMapper {
+    public static UserDto mapToEmployeeDto(User user) {
+        return new UserDto(
+                user.getId(),
+                user.getUserId(),
+                user.getPseudo(),
+                user.getEmail(),
+                user.getProfileImg(),
+                user.getPost()
+        );
+    }
+
+    public static User mapToUser(UserDto userDto) {
+        User user = new User();
+
+        user.setId(userDto.getId());
+        user.setUserId(userDto.getUserId());
+        user.setPseudo(userDto.getPseudo());
+        user.setEmail(userDto.getEmail());
+        user.setProfileImg(userDto.getProfileImg());
+        user.setPost(userDto.getPost());
+
+        return user;
+    }
+}
