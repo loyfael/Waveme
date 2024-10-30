5ac07ef07ad32770912a9d34d097592c8123c44a
Loïs <58104051+loyfael@users.noreply.github.com>
2024-10-30 16:30:09 +0100

    feat(backend): add roles

---
+++ waveme-backend/src/main/java/fr/waveme/backend/crud/models/Role.java
@@ -0,0 +1,25 @@
+package fr.waveme.backend.crud.models;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+}
