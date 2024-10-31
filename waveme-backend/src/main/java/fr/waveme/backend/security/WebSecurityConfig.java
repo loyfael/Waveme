dcbd17882a2b4c8c8b1b4aaa14530d5d8e96d620
Loïs <58104051+loyfael@users.noreply.github.com>
2024-10-31 10:26:10 +0100

    minor(security): update name

---
+++ waveme-backend/src/main/java/fr/waveme/backend/security/WebSecurityConfig.java
@@ -0,0 +1,88 @@
+package fr.waveme.backend.security;
+
+import fr.waveme.backend.security.jwt.AuthEntryPointJwt;
+import fr.waveme.backend.security.jwt.AuthTokenFilter;
+import fr.waveme.backend.security.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+// Indique a Spring quelle classe appliquer à la sécu
+@Configuration
+// permet d'autoriser les méthodes @PreAuthorize, @PostAuthorize, @Secured et @RolesAllowed.
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    // On construit une autentification des users en base de données.
+    // Quel service le fourni?
+    // Quel hasher utilisé?
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    // permet d'activer l'utilisation simplifié de l'autentification
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    // défini le hasher de psw par défaut.
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // on indique comment les CORS sont gérés
+    // Quel gestionnaire gère les exceptions d'accès
+    // La politique de session
+    // Les autorisations globales sur certaines URI
+    // On lui dit qu'elle fournisseur d'auth utiliser
+    // on ajoute un fitlre qui va verifier si un token est présent dans la requête avant que la requête arrive sur le controleur.
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/api/test/**").permitAll()
+                                .anyRequest().authenticated()
+                );
+
+        http.authenticationProvider(authenticationProvider());
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
+}
