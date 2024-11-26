21d9659549e058756b9965cb840cb45544497b28
Loïs <58104051+loyfael@users.noreply.github.com>
2024-11-26 12:10:55 +0100

    feat(backend:storage): add minio config

---
+++ waveme-backend/src/main/java/fr/waveme/backend/storage/MinioConfig.java
@@ -0,0 +1,24 @@
+package fr.waveme.backend.storage;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+public class MinioConfig {
+    @Value("${minio.endpoint}")
+    private String endpoint;
+
+    @Value("${minio.access-key}")
+    private String accessKey;
+
+    @Value("${minio.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+}
