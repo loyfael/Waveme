10d12524b839eb78029443ee4873aa69f12aa1d5
Loïs <felke2101@gmail.com>
2025-05-03 21:17:05 +0200

    feat: update PostRepository

--- waveme-backend/src/main/java/fr/waveme/backend/crud/repository/PostRepository.java
+++ waveme-backend/src/main/java/fr/waveme/backend/crud/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package fr.waveme.backend.crud.repository;
 
 import fr.waveme.backend.crud.models.Post;
+import fr.waveme.backend.crud.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.swing.text.html.Option;
@@ -9,4 +10,5 @@ import java.util.Optional;
 public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findByImageUrl(String imageUrl);
     Optional<Post> findByImageUrlContaining(String partOfImageUrl);
+    Optional<Post> findByUser(User user);
 }
