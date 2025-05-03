e932ed41f714f5ea6c009c9604476fa069e48b9a
Loïs <felke2101@gmail.com>
2025-05-03 21:18:31 +0200

    feat: add publicDto for user

---
+++ waveme-backend/src/main/java/fr/waveme/backend/crud/dto/pub/UserPublicDto.java
@@ -0,0 +1,24 @@
+package fr.waveme.backend.crud.dto.pub;
+
+import java.time.LocalDateTime;
+
+public class UserPublicDto {
+  public Long id;
+  public String pseudo;
+  public String email;
+  public String profileImg;
+  public LocalDateTime createdAt;
+  public LocalDateTime updatedAt;
+  public int totalUpvotes;
+
+  public UserPublicDto(Long id, String pseudo, String email, String profileImg,
+                       LocalDateTime createdAt, LocalDateTime updatedAt, int totalUpvotes) {
+    this.id = id;
+    this.pseudo = pseudo;
+    this.email = email;
+    this.profileImg = profileImg;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.totalUpvotes = totalUpvotes;
+  }
+}
