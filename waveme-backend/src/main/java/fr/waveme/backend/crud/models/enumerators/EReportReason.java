d71f79796560011ccbfa1707d3ff5a39b2a52888
Loïs <58104051+loyfael@users.noreply.github.com>
2025-05-20 16:37:54 +0200

    fix(enum): complicated

--- waveme-backend/src/main/java/fr/waveme/backend/crud/models/enumerators/EReportReason.java
+++ waveme-backend/src/main/java/fr/waveme/backend/crud/models/enumerators/EReportReason.java
@@ -1,23 +1,13 @@
 package fr.waveme.backend.crud.models.enumerators;
 
-import lombok.Getter;
-
 /**
  * Enum representing the reasons for reporting a user.
  * Each reason is associated with a string description.
  */
-@Getter
 public enum EReportReason {
-    SPAM("Spam"),
-    HARASSMENT("Harassment"),
-    INAPPROPRIATE_CONTENT("Inappropriate Content"),
-    IMPERSONATION("Impersonation"),
-    OTHER("Other");
-
-    private final String reason;
-
-    EReportReason(String reason) {
-        this.reason = reason;
-    }
-
+    SPAM,
+    HARASSMENT,
+    INAPPROPRIATE_CONTENT,
+    IMPERSONATION,
+    OTHER
 }
