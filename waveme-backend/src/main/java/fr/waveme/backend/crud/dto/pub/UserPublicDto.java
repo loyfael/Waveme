3fef6b64afc4f693ae633074d2904a9d5116a7f6
Loïs <58104051+loyfael@users.noreply.github.com>
2025-05-21 11:25:08 +0200

    feat(pub): add totalposts

--- waveme-backend/src/main/java/fr/waveme/backend/crud/dto/pub/UserPublicDto.java
+++ waveme-backend/src/main/java/fr/waveme/backend/crud/dto/pub/UserPublicDto.java
@@ -6,17 +6,19 @@ public class UserPublicDto {
   public Long id;
   public String pseudo;
   public String profileImg;
+  public int totalUpvotes;
+  public int totalPosts;
   public LocalDateTime createdAt;
   public LocalDateTime updatedAt;
-  public int totalUpvotes;
 
-  public UserPublicDto(Long id, String pseudo, String profileImg,
-                       LocalDateTime createdAt, LocalDateTime updatedAt, int totalUpvotes) {
+  public UserPublicDto(Long id, String pseudo, String profileImg, int totalUpvotes, int totalPosts,
+                       LocalDateTime createdAt, LocalDateTime updatedAt) {
     this.id = id;
     this.pseudo = pseudo;
     this.profileImg = profileImg;
+    this.totalUpvotes = totalUpvotes;
+    this.totalPosts = totalPosts;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
-    this.totalUpvotes = totalUpvotes;
   }
 }
