845d51128ac57316076f58a248a28c32c98605da
Loïs <58104051+loyfael@users.noreply.github.com>
2025-05-21 16:37:11 +0200

    remove poc

--- waveme-backend/src/main/java/fr/waveme/backend/crud/repository/ReportRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.waveme.backend.crud.repository;
-
-import fr.waveme.backend.crud.models.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReportRepository extends JpaRepository<Report, Long> {
-}
