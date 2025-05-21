08ffeab7ffa369fa7963a5052a39df54f9ff08d5
Loïs <58104051+loyfael@users.noreply.github.com>
2025-05-21 11:27:11 +0200

    fix(repository): findByUser return a Optional List

--- waveme-backend/src/main/java/fr/waveme/backend/crud/repository/PostRepository.java
+++ waveme-backend/src/main/java/fr/waveme/backend/crud/repository/PostRepository.java
@@ -5,10 +5,11 @@ import fr.waveme.backend.crud.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findByImageUrl(String imageUrl);
     Optional<Post> findByImageUrlContaining(String partOfImageUrl);
-    Optional<Post> findByUser(User user);
+    List<Post> findByUser(User user);
 }
