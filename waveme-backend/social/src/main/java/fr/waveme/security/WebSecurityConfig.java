bbd55057c713c8544c57836c34559070011291e2
Loïs <58104051+loyfael@users.noreply.github.com>
2025-05-23 17:01:19 +0200

    feat: add JwtAuthFiters

--- waveme-backend/social/src/main/java/fr/waveme/security/WebSecurityConfig.java
+++ waveme-backend/social/src/main/java/fr/waveme/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package fr.waveme.security;
 
 
+import fr.waveme.security.jwt.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
