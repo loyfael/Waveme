b1df2a05e67a40efdafafb9c7eda86494039b595
Loïs <58104051+loyfael@users.noreply.github.com>
2025-05-26 14:18:19 +0200

    feat: update

--- waveme-backend/core/src/main/java/fr/waveme/Core.java
+++ waveme-backend/core/src/main/java/fr/waveme/Core.java
@@ -1,11 +1,14 @@
 package fr.waveme;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import fr.waveme.storage.MinioProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@SpringBootApplication(scanBasePackages = "fr.waveme")
+@EnableConfigurationProperties(MinioProperties.class)
 public class Core {
     public static void main(String[] args) {
-        new SpringApplicationBuilder()
-                .sources(Auth.class, Social.class)
-                .run(args);
+        SpringApplication.run(Core.class, args);
     }
 }
