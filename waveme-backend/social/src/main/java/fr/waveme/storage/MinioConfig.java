f7fe9f54c33fddf1b70eb7662fa1de62150f3509
Loïs <58104051+loyfael@users.noreply.github.com>
2025-05-26 14:19:21 +0200

    feat: add debug

--- waveme-backend/social/src/main/java/fr/waveme/storage/MinioConfig.java
+++ waveme-backend/social/src/main/java/fr/waveme/storage/MinioConfig.java
@@ -1,6 +1,7 @@
 package fr.waveme.storage;
 
 import io.minio.MinioClient;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +21,11 @@ public class MinioConfig {
                 .endpoint(minioProperties.getEndpoint())
                 .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
                 .build();
+    }
+
+    @PostConstruct
+    public void debug() {
+        System.out.println("MinIO endpoint: " + minioProperties.getEndpoint());
     }
 }
 
