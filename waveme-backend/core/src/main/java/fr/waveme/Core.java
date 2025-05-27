6a13fdb2b633aacb404ae8ea0ca3538e37fb8b98
Loïs <58104051+loyfael@users.noreply.github.com>
2025-05-27 15:47:47 +0200

    test: detect bean

--- waveme-backend/core/src/main/java/fr/waveme/Core.java
+++ waveme-backend/core/src/main/java/fr/waveme/Core.java
@@ -4,8 +4,13 @@ import fr.waveme.storage.MinioProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = "fr.waveme")
+@SpringBootApplication(scanBasePackages = {
+        "fr.waveme.auth",
+        "fr.waveme.social",
+        "fr.waveme.core"
+})
 @EnableConfigurationProperties(MinioProperties.class)
 public class Core {
     public static void main(String[] args) {
