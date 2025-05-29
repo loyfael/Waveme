40191522bc92e27c7593eff4bc90793ef269a1fa
Loïs <felke2101@gmail.com>
2025-05-29 10:35:46 +0200

    feat: update repository for mongodb

--- waveme-backend/src/main/java/fr/waveme/backend/crud/repository/ReplyRepository.java
+++ waveme-backend/src/main/java/fr/waveme/backend/crud/repository/ReplyRepository.java
@@ -1,12 +1,13 @@
 package fr.waveme.backend.crud.repository;
 
 import fr.waveme.backend.crud.models.Reply;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ReplyRepository extends JpaRepository<Reply, Long> {
+public interface ReplyRepository extends MongoRepository<Reply, Long> {
     Optional<Reply> findByDescription(String description);
     Optional<Reply> findByDescriptionContaining(String partOfDescription);
     List<Reply> findByUserId(String userId);
