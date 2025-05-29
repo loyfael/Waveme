fd22839ecb50555cb4bae5213cf2ad2a3e8e7de4
Loïs <felke2101@gmail.com>
2025-05-29 10:35:39 +0200

    feat: update repository for mongodb

--- waveme-backend/src/main/java/fr/waveme/backend/crud/repository/CommentRepository.java
+++ waveme-backend/src/main/java/fr/waveme/backend/crud/repository/CommentRepository.java
@@ -1,12 +1,12 @@
 package fr.waveme.backend.crud.repository;
 
 import fr.waveme.backend.crud.models.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends MongoRepository<Comment, Long> {
     Optional<Comment> findByDescription(String description);
     Optional<Comment> findByDescriptionContaining(String partOfDescription);
     List<Comment> findByUserId(Long userId);
