f97ac1075a086f6ab7e0db84d64e6577d47e0fa3
Loïs <felke2101@gmail.com>
2025-05-29 10:35:17 +0200

    feat: update entity for mongodb

--- waveme-backend/src/main/java/fr/waveme/backend/crud/models/Comment.java
+++ waveme-backend/src/main/java/fr/waveme/backend/crud/models/Comment.java
@@ -5,47 +5,29 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "comment")
+@Document(collection = "comments")
 public class Comment {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id", nullable = false)
     private Long userId;
-
-    @Column(name = "upvote")
     private Integer upVote;
-
-    @Column(name = "downvote")
     private Integer downVote;
-
-    @Column(name = "description")
     private String description;
 
-    @ManyToOne
-    @JoinColumn(name = "post_id", nullable = false)
-    private Post post;
+    private String postId; // référence au post parent
+    private List<Long> replyIds; // références aux réponses
 
-    @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<Reply> reply;
-
-    @CreationTimestamp
-    @Column(nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime createdAt;
-
-    @UpdateTimestamp
-    @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime updatedAt;
 }
