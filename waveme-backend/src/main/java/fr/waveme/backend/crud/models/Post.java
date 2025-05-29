b71574d9b28b563732f8788e125164c2bcf39096
Loïs <felke2101@gmail.com>
2025-05-29 10:35:24 +0200

    feat: update entity for mongodb

--- waveme-backend/src/main/java/fr/waveme/backend/crud/models/Post.java
+++ waveme-backend/src/main/java/fr/waveme/backend/crud/models/Post.java
@@ -5,10 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 
 @Getter
@@ -19,36 +18,18 @@ import java.util.Set;
 @Table(name = "post")
 public class Post {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "image_url", nullable = false, length = 1000)
     private String imageUrl;
-
-    @Column(name = "upvote")
     private Integer upVote;
-
-    @Column(name = "downvote")
     private Integer downVote;
-
-    @Column(name = "description")
     private String description;
-
-    @Column(name ="shorturl", unique = true)
     private String shortUrl;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    private Long userId; // refering to the user who created the post
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<Comment> comments;
+    private List<Long> commentIds; // refering to the comments on this post
 
-    @CreationTimestamp
-    @Column(nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime createdAt;
-
-    @UpdateTimestamp
-    @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime updatedAt;
 }
