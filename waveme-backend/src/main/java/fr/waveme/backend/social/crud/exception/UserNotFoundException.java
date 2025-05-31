475025e26010af5d07f16cd2adc883f0f87beab8
Loïs <felke2101@gmail.com>
2025-05-31 10:40:41 +0200

    feat(exceptions): add

---
+++ waveme-backend/src/main/java/fr/waveme/backend/social/crud/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package fr.waveme.backend.social.crud.exception;
+
+public class UserNotFoundException extends RuntimeException {
+  public UserNotFoundException(String message) {
+    super(message);
+  }
+}
