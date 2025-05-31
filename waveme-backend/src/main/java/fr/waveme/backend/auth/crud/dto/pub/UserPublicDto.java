b2e4872e1092eb4a732a3568c6fbdc3a7f5c4bb2
Loïs <felke2101@gmail.com>
2025-05-31 17:22:35 +0200

    fix: remove unused fields

--- waveme-backend/src/main/java/fr/waveme/backend/auth/crud/dto/pub/UserPublicDto.java
+++ waveme-backend/src/main/java/fr/waveme/backend/auth/crud/dto/pub/UserPublicDto.java
@@ -5,19 +5,14 @@ import java.time.LocalDateTime;
 public class UserPublicDto {
   public Long id;
   public String pseudo;
-  public String profileImg;
   public int totalUpvotes;
   public int totalPosts;
   public LocalDateTime createdAt;
   public LocalDateTime updatedAt;
 
-  public UserPublicDto(Long id, String pseudo, String profileImg, int totalUpvotes, int totalPosts,
-                       LocalDateTime createdAt, LocalDateTime updatedAt) {
+  public UserPublicDto(Long id, String pseudo, LocalDateTime createdAt, LocalDateTime updatedAt) {
     this.id = id;
     this.pseudo = pseudo;
-    this.profileImg = profileImg;
-    this.totalUpvotes = totalUpvotes;
-    this.totalPosts = totalPosts;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
   }
