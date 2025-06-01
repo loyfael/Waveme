d994db72f7fdd5263aa5a65d5df2ee7fb41db887
Loïs <felke2101@gmail.com>
2025-06-01 15:58:11 +0200

    fix: constructor and email missing

--- waveme-backend/src/main/java/fr/waveme/backend/auth/crud/dto/pub/UserPublicDto.java
+++ waveme-backend/src/main/java/fr/waveme/backend/auth/crud/dto/pub/UserPublicDto.java
@@ -1,19 +1,20 @@
 package fr.waveme.backend.auth.crud.dto.pub;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserPublicDto {
   public Long id;
   public String pseudo;
-  public int totalUpvotes;
-  public int totalPosts;
+  public String email;
   public LocalDateTime createdAt;
   public LocalDateTime updatedAt;
-
-  public UserPublicDto(Long id, String pseudo, LocalDateTime createdAt, LocalDateTime updatedAt) {
-    this.id = id;
-    this.pseudo = pseudo;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
-  }
 }
