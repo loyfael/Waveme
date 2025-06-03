edc7fe62ce41afeda625c8d725132c2bd4082ae6
Loïs <58104051+loyfael@users.noreply.github.com>
2025-06-03 14:33:29 +0200

    feat(docs): add comments

--- waveme-backend/src/main/java/fr/waveme/backend/auth/crud/repository/RoleRepository.java
+++ waveme-backend/src/main/java/fr/waveme/backend/auth/crud/repository/RoleRepository.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * RoleRepository is a Spring Data JPA repository interface for managing Role entities.
+ * It extends JpaRepository to provide CRUD operations and custom query methods.
+ * This repository is used to interact with the database for role-related operations.
+ */
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(ERole name);
