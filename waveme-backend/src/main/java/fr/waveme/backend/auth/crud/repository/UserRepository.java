5daafd1922ec1e5e3ef7c40520be14e1e5dc4d4e
Loïs <58104051+loyfael@users.noreply.github.com>
2025-06-03 14:33:37 +0200

    feat(docs): add comments

--- waveme-backend/src/main/java/fr/waveme/backend/auth/crud/repository/UserRepository.java
+++ waveme-backend/src/main/java/fr/waveme/backend/auth/crud/repository/UserRepository.java
@@ -5,6 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * UserRepository is a Spring Data JPA repository interface for managing User entities.
+ * It extends JpaRepository to provide CRUD operations and custom query methods.
+ * This repository is used to interact with the database for user-related operations.
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByPseudo(String pseudo);
 
