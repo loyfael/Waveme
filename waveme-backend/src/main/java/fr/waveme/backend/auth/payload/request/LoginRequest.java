8bffbe5de4b3ee3a491e1e2e3793a4ee8ea849d2
Loïs <58104051+loyfael@users.noreply.github.com>
2025-06-03 14:34:23 +0200

    feat(docs): add comments

--- waveme-backend/src/main/java/fr/waveme/backend/auth/payload/request/LoginRequest.java
+++ waveme-backend/src/main/java/fr/waveme/backend/auth/payload/request/LoginRequest.java
@@ -5,6 +5,11 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * LoginRequest is a class that represents the request payload for user login.
+ * It contains fields for the user's pseudo and password, both of which are required.
+ * This class is used to validate the input data when a user attempts to log in.
+ */
 @Setter
 @Getter
 public class LoginRequest {
