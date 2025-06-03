632e9c9e3ffb5dd74402dde8d7d152d7949f1b86
Loïs <58104051+loyfael@users.noreply.github.com>
2025-06-03 14:33:15 +0200

    feat(docs): add comments

--- waveme-backend/src/main/java/fr/waveme/backend/auth/crud/models/enumerators/ERole.java
+++ waveme-backend/src/main/java/fr/waveme/backend/auth/crud/models/enumerators/ERole.java
@@ -1,5 +1,10 @@
 package fr.waveme.backend.auth.crud.models.enumerators;
 
+/**
+ * ERole is an enumeration that defines the different roles a user can have in the application.
+ * It includes three roles: USER, MODERATOR, and ADMIN.
+ * This enumeration is used to manage user permissions and access levels within the application.
+ */
 public enum ERole {
     ROLE_USER,
     ROLE_MODERATOR,
