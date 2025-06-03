8735442e6157911c1cc77c4498c111bb95cea645
Loïs <58104051+loyfael@users.noreply.github.com>
2025-06-03 14:34:31 +0200

    feat(docs): add comments

--- waveme-backend/src/main/java/fr/waveme/backend/auth/payload/request/UserProfileRegisterRequest.java
+++ waveme-backend/src/main/java/fr/waveme/backend/auth/payload/request/UserProfileRegisterRequest.java
@@ -3,6 +3,11 @@ package fr.waveme.backend.auth.payload.request;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * UserProfileRegisterRequest is a class that represents the request payload for user profile registration.
+ * It contains fields for the authenticated user's ID, pseudo, and email.
+ * This class is used to validate the input data when a user registers their profile.
+ */
 @Getter
 @Setter
 /**
