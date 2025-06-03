99b5c473b3cd9b08ea170dc98c1a55b645f5a21f
Loïs <58104051+loyfael@users.noreply.github.com>
2025-06-03 14:33:12 +0200

    feat(docs): add comments

--- waveme-backend/src/main/java/fr/waveme/backend/auth/crud/mapper/UserMapper.java
+++ waveme-backend/src/main/java/fr/waveme/backend/auth/crud/mapper/UserMapper.java
@@ -3,6 +3,11 @@ package fr.waveme.backend.auth.crud.mapper;
 import fr.waveme.backend.auth.crud.dto.UserDto;
 import fr.waveme.backend.auth.crud.models.User;
 
+/**
+ * UserMapper is a utility class that provides methods to convert between User and UserDto objects.
+ * It contains static methods to map User to UserDto and vice versa.
+ * This is useful for separating the data representation used in the application from the database entity.
+ */
 public class UserMapper {
     public static UserDto mapToUserDto(User user) {
         return new UserDto(
