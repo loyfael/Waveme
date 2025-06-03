657908e078ae9f05aaa66f69c23ecd93e0d1c435
Loïs <58104051+loyfael@users.noreply.github.com>
2025-06-03 14:34:41 +0200

    feat(docs): add comments

--- waveme-backend/src/main/java/fr/waveme/backend/auth/payload/response/MessageResponse.java
+++ waveme-backend/src/main/java/fr/waveme/backend/auth/payload/response/MessageResponse.java
@@ -3,6 +3,10 @@ package fr.waveme.backend.auth.payload.response;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * MessageResponse is a class that represents a simple response containing a message.
+ * It is typically used to send feedback to the client, such as success or error messages.
+ */
 @Setter
 @Getter
 public class MessageResponse {
