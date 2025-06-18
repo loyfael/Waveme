6897f1d6b65c875304894212103c8a2fd9d9b468
Loïs <58104051+loyfael@users.noreply.github.com>
2025-06-18 14:47:27 +0200

    fix(request): getter/setter missings

--- waveme-backend/src/main/java/fr/waveme/backend/auth/payload/request/LoginRequest.java
+++ waveme-backend/src/main/java/fr/waveme/backend/auth/payload/request/LoginRequest.java
@@ -1,21 +1,30 @@
 package fr.waveme.backend.auth.payload.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 /**
  * LoginRequest is a class that represents the request payload for user login.
  * It contains fields for the user's pseudo and password, both of which are required.
  * This class is used to validate the input data when a user attempts to log in.
  */
-@Setter
+@Data
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     @NotBlank
     private String pseudo;
 
     @NotBlank
     private String password;
+
+    public void setUsername(String user) {
+        this.pseudo = user;
+    }
+
+    public String getUsername() {
+        return pseudo;
+    }
 }
