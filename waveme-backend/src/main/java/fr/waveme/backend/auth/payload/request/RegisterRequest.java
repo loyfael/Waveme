227a6673e4029dcd429d1989fe9fc92070e3d797
Loïs <58104051+loyfael@users.noreply.github.com>
2025-06-18 14:47:49 +0200

    fix(request): decorator missings

--- waveme-backend/src/main/java/fr/waveme/backend/auth/payload/request/RegisterRequest.java
+++ waveme-backend/src/main/java/fr/waveme/backend/auth/payload/request/RegisterRequest.java
@@ -1,16 +1,18 @@
 package fr.waveme.backend.auth.payload.request;
 
 import jakarta.validation.constraints.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 /**
  * RegisterRequest is a class that represents the request payload for user registration.
  * It contains fields for the user's pseudo, email, and password, all of which are required.
  * This class is used to validate the input data when a user attempts to register.
  */
+@Data
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
     @NotBlank
     @Size(min = 3, max = 20)
