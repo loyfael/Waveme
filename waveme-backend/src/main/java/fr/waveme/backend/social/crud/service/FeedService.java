348db9791b27893fb99ac898182464a5b5ca2e1a
Loïs <felke2101@gmail.com>
2025-06-22 20:54:41 +0200

    fix: bad system

--- waveme-backend/src/main/java/fr/waveme/backend/social/crud/service/FeedService.java
+++ waveme-backend/src/main/java/fr/waveme/backend/social/crud/service/FeedService.java
@@ -1,8 +1,8 @@
 package fr.waveme.backend.social.crud.service;
 
-import fr.waveme.backend.social.crud.dto.PostFeedDto;
+import fr.waveme.backend.social.crud.dto.pub.post.PostMetadataDto;
 import org.springframework.data.domain.Page;
 
 public interface FeedService {
-  Page<PostFeedDto> getLatestPosts(int page, int size);
+  Page<PostMetadataDto> getFeedMetadata(int page);
 }
