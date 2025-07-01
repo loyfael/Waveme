3d3f39216386bd1a1d040bbeb47176c66c9839f5
Loïs <felke2101@gmail.com>
2025-07-01 08:24:51 +0200

    Remove port from BASE_SERVER_URL constant
    Updated BASE_SERVER_URL in API.ts to exclude the port 9998, likely reflecting a server configuration change or migration to the default HTTPS port.

--- waveme-app/constants/API.ts
+++ waveme-app/constants/API.ts
@@ -1,5 +1,5 @@
 // Yes, this is the real server IP. But i'm not sysadmin, i'm developer, and i'm too lazy to update this just for one day
-export const BASE_SERVER_URL = 'https://45.140.164.224:9998'
+export const BASE_SERVER_URL = 'https://45.140.164.224'
 
 export const API_URL = `${BASE_SERVER_URL}/api`
 
